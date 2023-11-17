29d25116c1f04573dea3d05dbaaa5ef22f021593
torpi <torpi@DESKTOP-1K07F3U>
2023-11-17 08:30:12 +0330

    replace use of regular fetch()

---
+++ src/app/api/get-number/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
+export async function GET() {
+  const randomNumber = Math.random();
+//   console.log(randomNumber);
+  return NextResponse.json({ randomNumber });
+}
--- src/utils/getNumber.ts
+++ src/utils/getNumber.ts
@@ -3,12 +3,25 @@ function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 export const GetCustomNumber = unstable_cache(async () => {
-//   await sleep(100);
+  //   await sleep(100);
   const randomNumber = Math.random();
   return randomNumber;
-});
-export const GetCachedNumber = unstable_cache(async () => {
+},["customNumber"]);
+export const GetCachedNusmber = unstable_cache(async () => {
   //   await sleep(100);
-    const randomNumber = Math.random();
-    return randomNumber;
+  const randomNumber = Math.random();
+  return randomNumber;
+},["cachedNumber"]);
+export async function GetCachedNumber() {
+  // URL="http://localhost:3000"
+  // URL = "https://nextjs-revalidation-test.vercel.app"
+  const res = await fetch(process.env.URL + "/api/get-number", {
+    next: { tags: ["cachedNumber"] },
+    cache: "force-cache",
   });
+  if (!res.ok) {
+    throw new Error("ERROR_OCCURRED");
+  }
+  const json = await res.json();
+  return json.randomNumber;
+}
