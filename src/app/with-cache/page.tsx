6c0516c1d8552bc0e04f2583c4da32f7b2e8781f
torpi <torpi@company>
2023-11-18 21:29:10 +0330

    rename variable names

--- src/app/with-cache/page.tsx
+++ src/app/with-cache/page.tsx
@@ -3,15 +3,15 @@ import ServerNumber from "@/components/ServerNumber";
 import { GetCustomCachedNumber, GetUnsatableCachedNumber } from "@/utils/getNumber";
 
 export default async function Page() {
-  const number = await GetCustomCachedNumber();
-  const cached = await GetUnsatableCachedNumber();
+  const custom_number = await GetCustomCachedNumber();
+  const cached_number = await GetUnsatableCachedNumber();
   return (
     <>
       <p className="text-lg font-bold">Unstable_Cache</p>
       <ServerNumber />
-      Custom Cache : <CustomNumber rnd={number} />
+      Custom Cache : <CustomNumber rnd={custom_number} />
       <br/>
-      Layout Cache : <CustomNumber rnd={cached} />
+      Layout Cache : <CustomNumber rnd={cached_number} />
     </>
   );
 }
--- src/components/CachedNumber.tsx
+++ src/components/CachedNumber.tsx
@@ -2,6 +2,6 @@ import { GetUnsatableCachedNumber } from "@/utils/getNumber";
 import CustomNumber from "./CustomNumber";
 
 export default async function CachedNumber() {
-  const number = await GetUnsatableCachedNumber();
-  return <CustomNumber rnd={number} />;
+  const unstable_cached_number = await GetUnsatableCachedNumber();
+  return <CustomNumber rnd={unstable_cached_number} />;
 }
