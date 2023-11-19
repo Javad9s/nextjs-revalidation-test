3d79b96335203861860b57eff3c21bcfbc08156e
torpi <torpi@company>
2023-11-19 05:13:52 +0330

    hardcode env params to bash file

--- get-sc-creds.sh
+++ get-sc-creds.sh
@@ -1,4 +1,4 @@
-get_path="$NEXT_PUBLIC_SITE_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
+get_path="https://nextjs-revalidation-test.vercel.app/api/get-sc-creds?BUILD_SECRET=3be3505c-867c-11ee-b9d1-0242ac120002"
 response=$(curl -s $get_path)
 # If we got a valid response, then export to process
 if [[ "$response" == *"SUSPENSE_CACHE_"* ]]; then
