4f7a320946c04cc8b7c29b0c6528f6dad1e93366
torpi <torpi@company>
2023-11-30 16:43:19 +0330

    fix typo in notfound revalidation

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -10,7 +10,7 @@ export async function RevalidateISR1hPage() {
 
 }
 export async function RevalidateNotFound() {
-  revalidatePath("/_not-found ");
+  revalidatePath("/_not-found");
 }
 export async function RevalidateCacheTag() {
   revalidateTag("cashedNumber");
