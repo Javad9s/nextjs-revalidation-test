31ba70a29fda9cd88015de2f074a828385c4e294
torpi <torpi@company>
2023-12-01 08:00:34 +0330

    add time now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import CachedNumber from "@/components/CachedNumber";
+import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +16,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const AppTimeNow = dynamic(() => import("@/components/TimeNow"), {
+    ssr: false,
+    loading: () => <>Current Time...</>,
+  });
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <div className="flex items-center justify-center">
+          <AppTimeNow />
+        </div>
         <div className="flex flex-col items-center justify-center gap-4 p-8">
           {children}
         </div>
---
+++ src/components/TimeNow.tsx
@@ -0,0 +1,7 @@
+"use client";
+export default function TimeNow() {
+  const currentDate = new Date();
+  const utcDate = currentDate.toISOString().slice(0, 10);
+  const utcTime = currentDate.toISOString().slice(11, 19);
+  return <>{`Now: ${utcDate} ${utcTime} UTC`}</>;
+}
