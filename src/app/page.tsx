9580fea4dd4bdcf862d132d1e75d5088c46a2896
torpi <torpi@company>
2023-12-01 09:47:25 +0330

    enhance readability of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
       <p className="text-lg font-bold">Next v14.0.4-canary.31</p>
       <div className="items-start">
         <p className="text-lg font-bold">Steps to reproduce</p>
-        <p className="text-lg font-bold">Issue 1</p>
+        <br />
+        <p className="text-lg font-bold">Issue 1 (Soft navigation Rerender)</p>
         <p>1. On first deploy of the project</p>
         <p>
           2. (On Vercel) notice how cached values are inconsistence between
@@ -19,7 +20,9 @@ export default function Home() {
             This repo fixes it
           </a>
         </p>
-        <p>2. Go to Control Panel click on root layout.</p>
+        <p>
+          2. Go to &quot;Control Panel&quot; click on &quot;root layout&quot;.
+        </p>
         <p>
           3.1. (On Vercel) with soft navigation some of pages will still return
           stale data until first hard refresh on that specific page.
@@ -34,23 +37,36 @@ export default function Home() {
           revalidation mostly works as expected.
         </p>
         <p>3.5. (On local build) soft navigation returns updated data.</p>
-        <p className="text-lg font-bold">Issue 2</p>
-        <p>1. Go to Control Panel click on the root layout button.</p>
-        <p>2. Click on not found button on navbar or type in any arbitrary address.</p>
-        <p>3.1. (On Vercel) general not found page always returns stale data.</p>
+        <br />
+        <p className="text-lg font-bold">
+          Issue 2 (can&apos;t revalidate not found page)
+        </p>
+        <p>
+          1. Go to &quot;Control Panel&quot; click on the &quot;root
+          layout&quot; button.
+        </p>
+        <p>
+          2. Click on &quot;not found&quot; button on navbar or type in any
+          arbitrary address.
+        </p>
+        <p>
+          3.1. (On Vercel) general not found page always returns stale data.
+        </p>
         <p>3.2. (On local build) not found page is a dynamic page.</p>
-        <p className="text-lg font-bold">Issue 3</p>
-        <p>1. Type in Address /params/30 to /params/59.</p>
+        <br />
+        <p className="text-lg font-bold">Issue 3 (cached notfound())</p>
+        <p>1. Type in any Address between /params/30 to /params/59.</p>
         <p>
           2. Pages that use notFound() from next/navigation are actually cached.
         </p>
         <p>3. This could cause DoS of type Disk exhaustion.</p>
-        <p>4. notFound() should always redirect to one general not found page.</p>
         <p>
-          5. Must implement a garbage collector mechanism with Maximum cache size
-          and maximum cache age that purges oldest visited page.
+          4. notFound() should always redirect to one general not found page.
         </p>
-        
+        <p>
+          5. Must implement a garbage collector mechanism with Maximum cache
+          size and maximum cache age that purges oldest visited page.
+        </p>
       </div>
     </>
   );
