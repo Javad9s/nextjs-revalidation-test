405b3aa83e5764b777b1f7f9a67a25fddeacda4f
torpi <torpi@company>
2023-12-11 12:10:41 +0330

    fix typos

--- src/app/isr/2h/page.tsx
+++ src/app/isr/2h/page.tsx
@@ -2,9 +2,9 @@ import CachedNumber from "@/components/CachedNumber";
 import RandomNumber from "@/components/RandomNumber";
 import { renderLog } from "@/utils/utils";
 
-export const revalidate = 3600;
+export const revalidate = 7200;
 export default function Page() {
-  renderLog("Rendered ISR 1 h page.tsx");
+  renderLog("Rendered ISR 2 h page.tsx");
   return (
     <>
       <p className="text-lg font-bold">Revalidate every 2 hour</p>
